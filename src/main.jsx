c50371093d70d5dec1a1b3ca1b3ff2c8cab5e2bc
Zem860 <zemmyhammy@gmail.com>
2024-10-30 16:59:34 +0800

    global style 不是具名函式不要把它用大括號框起來

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import {GlobalStyle} from './GlobalStyle.jsx'
+import GlobalStyle from './GlobalStyle.jsx';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
