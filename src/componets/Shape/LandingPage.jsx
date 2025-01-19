2cd025ae8f18dd5a94abbe2cfa6759a58c727fef
Zem860 <zemmyhammy@gmail.com>
2025-01-19 10:37:00 +0800

    update:style adjustment

--- src/componets/Shape/LandingPage.jsx
+++ src/componets/Shape/LandingPage.jsx
@@ -2,10 +2,22 @@ import styled from "styled-components";
 
 const LandingPageStyle = styled.main
 `
-padding: 0 42 px
+padding:0 42px;
 `
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  background-color: #23272F;
+`;
+
 export const LandingPage = ({children}) => {
-    return ( <LandingPageStyle>{children}</LandingPageStyle> );
+    return ( 
+    <LandingPageStyle>
+        <Wrapper>
+        {children}
+        </Wrapper>
+    </LandingPageStyle> );
 }
  
